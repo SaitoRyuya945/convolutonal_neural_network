583a0e220fbcbe3e24a7d04feecfd8836ab16d25
SaitoRyuya945 <saitoryuya945@gmail.com>
2020-05-28 17:11:15 +0900

    アプリケーションのテストを追加

---
+++ tests/app/test_app.py
@@ -0,0 +1,46 @@
+"""
+メインのアプリ用テスト
+実際に実行したときにここに書かれているプログラムが動くようになっている。
+選択できる画像はjpg,pngで白黒画像のみを扱うことができる（カラーを選択した場合、
+自動で白黒になる）今後、RGBを扱うプログラムに変更していく。
+"""
+
+import pytest
+from PyQt5.QtWidgets import QApplication
+from convolutonal_neural_network import MatplotlibWindow
+import numpy as np
+
+
+class TestQtApplication(object):
+    """
+    class_name: QtApplication
+    description:
+        Qtを用いたアプリケーション
+    """
+    def setup_method(self, method):
+        print('method_start {}:'.format(method.__name__))
+        self.app = QApplication([])
+        self.window = MatplotlibWindow()
+        self.window.show()
+        self.app.exec_()
+
+    def teardown_method(self, method):
+        print('method_end {}:'.format(method.__name__))
+
+    def test_lowpass_fileter_select(self):
+        # 画像の読み込みテスト(jpg, pngのみ)
+        assert self.window.im != None
+        assert self.window.gray_im != None
+
+    def test_filter_select(self):
+        # フィルターの選択をテスト(HighPassFilterの選択)
+        assert self.window.filter_name == "HighPass"
+        assert self.window.kernel[0][0] == 0
+        assert self.window.kernel[0][1] == -3
+        assert self.window.kernel[0][2] == 0
+        assert self.window.kernel[1][0] == -3
+        assert self.window.kernel[1][1] == 13
+        assert self.window.kernel[1][2] == -3
+        assert self.window.kernel[2][0] == 0
+        assert self.window.kernel[2][1] == -3
+        assert self.window.kernel[2][2] == 0
